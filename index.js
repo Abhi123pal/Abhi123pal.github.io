65e1081dfcacabb03bf3eb7e1bc81c33a9e254cc
Abhi123pal <abhinavpal889@gmail.com>
2023-03-14 16:43:49 +0530

    responshive home page added index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector("nav i");
+const navUl = document.querySelector("nav ul");
+menu.onclick = () => {
+//   console.log(menu);
+  if (menu.classList.contains("bx-menu")) {
+    menu.classList.remove("bx-menu");
+    menu.classList.add("bx-x");
+    navUl.classList.add("active");
+  } else {
+    menu.classList.remove("bx-x");
+    navUl.classList.remove("active");
+    menu.classList.add("bx-menu");
+  }
+};
+
+// menu.classList.remove("bx-menu");
